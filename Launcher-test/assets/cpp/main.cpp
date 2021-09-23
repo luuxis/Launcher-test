0fccf672941e705db6e629997a82b40607268bf4
andre <61792917+luuxis@users.noreply.github.com>
2021-09-23 19:37:22 +0200

    Initial commit

---
+++ Launcher-test/assets/cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int download(std::string url, std::string destination) {
+    std::cout << "telechargement de minecraft\n";
+    system(("if not exist " + destination + " powershell -Command \"(New-Object Net.WebClient).DownloadFile('" + url + "', '" + destination + "')\" ").c_str());
+    return 0;
+}
+
+
+int main() {
+    std::cout << "Programme d'installation\n";
+    system("title minecraft");
+    system("if not exist %CD%\\Launcher md %CD%\\Launcher");
+    download("https://craftdium.ml/res/launcher/launcher.txt", "Launcher\\launcher.bat");
+    system("cd Launcher && launcher.bat");
+    return 0;
+}
