aaf62225fa03cf06aa33c1806cccecbdb1621e1f
andre <61792917+luuxis@users.noreply.github.com>
2021-09-23 23:02:26 +0200

    Update main.cpp

--- Launcher-test/assets/cpp/main.cpp
+++ Launcher-test/assets/cpp/main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 
 int download(std::string url, std::string destination) {
-    std::cout << "telechargement de minecraft\n";
     system(("powershell -Command \"(New-Object Net.WebClient).DownloadFile('" + url + "', '" + destination + "')\" ").c_str());
     return 0;
 }
 
 
 int main() {
-    std::cout << "Programme d'installation\n";
-    system("title minecraft");
+    system("title Launcher");
     system("if not exist %CD%\\Launcher md %CD%\\Launcher");
     download("https://craftdium.ml/res/launcher/launcher.txt", "Launcher\\launcher.bat");
     system("cd Launcher && launcher.bat");
